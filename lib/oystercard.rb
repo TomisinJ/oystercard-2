829a62def416d4ba57a4923c1fbabbc5d9c0e15b
tj <tomisinjoseph3@gmail.com>
2021-09-01 17:39:59 +0100

    Co-authored-by: Chloë Emily Stokes (she/they)  <Chloeem@users.noreply.github.com>

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,10 +1,14 @@
 class Oystercard
+  MAXIMUM_AMOUNT = 90
+
   attr_reader :balance
   def initialize
     @balance = 0
   end
 
   def top_up(amount)
+    fail "You have exceeded the maximum amount of £#{MAXIMUM_AMOUNT}" if amount + @balance > MAXIMUM_AMOUNT
     @balance += amount 
   end
+
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,13 +1,21 @@
 require 'oystercard'
 
 describe Oystercard do
-
   it 'has balance of zero' do
     expect(subject.balance).to eq (0)
   end
 
-  it 'tops up oystercard' do
-    expect{ subject.top_up(1) }.to change { subject.balance }.by(1)
+  describe "#top_up" do
+    it 'tops up oystercard' do
+      expect{ subject.top_up(1) }.to change { subject.balance }.by(1)
+    end
+
+    it 'raises an error when the maximum amount is exceeded' do
+      subject.top_up(Oystercard::MAXIMUM_AMOUNT)
+      expect { subject.top_up(Oystercard::MAXIMUM_AMOUNT) }.to raise_error "You have exceeded the maximum amount of £#{Oystercard::MAXIMUM_AMOUNT}"
+    end
+
   end
+
 
 end
